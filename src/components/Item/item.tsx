5c8a516c41e12a00782733d4342ed5aaae4053ca
Luca Morelli <luca.morelli.lm@gmail.com>
2022-11-23 11:49:31 +0100

    feat: add dynamic behaviour and look and feel to Item component

--- src/components/Item/item.tsx
+++ src/components/Item/item.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
 type PropsWithClassName<P = unknown> = P & { className?: string | undefined };
 
@@ -9,11 +10,41 @@ type Props = {
   text: string;
 };
 
-const Item = ({ checked, text, className }: PropsWithClassName<Props>) => (
-  <div className={clsx("p-5 bg-white rounded-lg flex gap-5 ", className)}>
-    <ArrowRightCircleIcon className="w-6 h-6 grow-0" />
-    <p className={checked ? "line-through" : ""}>{text}</p>
-  </div>
+const renderIcon = (checked: Props["checked"]) => (
+  <>
+    {checked ? (
+      <CheckCircleIcon className="w-6 h-6 grow-0" />
+    ) : (
+      <ArrowRightCircleIcon className="w-6 h-6 grow-0" />
+    )}
+  </>
 );
+const Item = ({
+  checked: isChecked,
+  text,
+  className,
+}: PropsWithClassName<Props>) => {
+  const [checked, setChecked] = useState(isChecked);
+
+  const handleClick = () => {
+    setChecked((wasChecked) => !wasChecked);
+  };
+
+  return (
+    <div
+      className={clsx(
+        "p-5 bg-white rounded-lg",
+        "flex gap-5",
+        checked ? "text-indigo-200" : "",
+        "cursor-pointer",
+        className
+      )}
+      onClick={handleClick}
+    >
+      {renderIcon(checked)}
+      <p className={checked ? "line-through" : ""}>{text}</p>
+    </div>
+  );
+};
 
 export { Item };
