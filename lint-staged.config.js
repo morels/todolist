b0da7dbcad2a56db2b7f4fd7358e6a15e30dd699
Luca Morelli <luca.morelli.lm@gmail.com>
2022-11-24 17:34:09 +0100

    build: install and setup husky and lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+// The following configuration was taken from https://nextjs.org/docs/basic-features/eslint#lint-staged
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{ts,tsx}': [buildEslintCommand],
+}
