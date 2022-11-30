561f0280dab25b7ea64623b0e757a3eb1372012e
Luca Morelli <luca.morelli.lm@gmail.com>
2022-11-30 02:19:40 +0100

    feat: css style

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import React from "react";
 
 const Title = ({ children }: React.PropsWithChildren) => (
-  <h1 className={clsx("m-0 leading-loose text-6xl font-bold")}>{children}</h1>
+  <h1 className={clsx("m-0 mb-12 leading-normal text-6xl font-bold")}>{children}</h1>
 );
 
 export { Title };
